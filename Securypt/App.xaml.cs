14cb564335515e3cffac998349013667a5af842b
Kevin Mitchell <kfmitch@gmail.com>
2017-11-05 21:45:29 -0800

    Revert "fix git"
    This reverts commit e5a3fbbec7bc346f5ee7a9fbe9d2bb9f0c2888f1.

--- Securypt/App.xaml.cs
+++ Securypt/App.xaml.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -16,22 +15,3 @@ namespace Securypt
 	{
 	}
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace Securypt
-{
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
-	{
-	}
-}
->>>>>>> b2824f01adee20c126fc1eb04829afdb778c79c8
--- Securypt/TutorialPage.xaml.cs
+++ Securypt/TutorialPage.xaml.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,33 +26,3 @@ namespace Securypt
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Securypt
-{
-    /// <summary>
-    /// Interaction logic for TutorialPage.xaml
-    /// </summary>
-    public partial class TutorialPage : Page
-    {
-        public TutorialPage()
-        {
-            InitializeComponent();
-        }
-    }
-}
->>>>>>> b2824f01adee20c126fc1eb04829afdb778c79c8
